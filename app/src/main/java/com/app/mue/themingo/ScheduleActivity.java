abd7f181b0e8c78c19e29c266e58a385a43446b3
Onikah <mona.kwamboka@gmail.com>
2016-03-17 19:48:22 +0300

    creating schedule list views

--- app/src/main/java/com/app/mue/themingo/ScheduleActivity.java
+++ app/src/main/java/com/app/mue/themingo/ScheduleActivity.java
@@ -13,18 +13,9 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        AnimatedListView animatedListView = (AnimatedListView) findViewById(R.id.my_list);
+
     }
 
 }
