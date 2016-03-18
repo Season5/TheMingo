c10f7ee9fb3fc121bc1fc085d55d6246397931a6
Onikah <mona.kwamboka@gmail.com>
2016-03-18 13:07:27 +0300

    pasing image for grid layout

---
+++ app/src/main/java/com/app/mue/themingo/adapters/MainAdapter.java
@@ -0,0 +1,106 @@
+package com.app.mue.themingo.adapters;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.app.mue.themingo.R;
+import com.app.mue.themingo.listeners.OnGridItemSelectedListener;
+
+
+public class MainAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPER_PAGER = 0;
+    private static final int TYPE_ITEM = 1;
+
+    private static final int HEADER_PAGER_LAYOUT = 1;
+
+    private final int mImageResID;
+
+    private Context mContext;
+
+
+    private OnGridItemSelectedListener mOnGridItemSelectedListener;
+
+    public MainAdapter(Context context, int imageResID) {
+        this.mImageResID = imageResID;
+        this.mContext = context;
+        mOnGridItemSelectedListener = (OnGridItemSelectedListener) mContext;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
+        if(viewType == TYPER_PAGER) {
+            View v = LayoutInflater.from(parent.getContext()).inflate (R.layout.include_view_pager, parent, false);
+            return new HeaderViewHolder (v);
+            
+        } else if(viewType == TYPE_ITEM) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_home, null);
+            return new HeaderViewHolder.RecyclerViewHolder(view);
+        }
+
+
+        return null;
+    }
+
+
+
+    @Override
+    public int getItemViewType (int position) {
+        if(isPositionHeader (position)) {
+            return TYPER_PAGER;
+        }
+        return TYPE_ITEM;
+    }
+
+    public boolean isPositionHeader(int position) {
+        return position == 0;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return (mContext.getResources().getIntArray(mImageResID).length) + HEADER_PAGER_LAYOUT;
+    }
+
+
+
+    public class HeaderViewHolder extends RecyclerView.ViewHolder {
+        ViewPager viewPager;
+
+        public HeaderViewHolder (View itemView) {
+            super (itemView);
+            this.viewPager = (ViewPager) itemView.findViewById(R.id.pager);
+
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public ImageView image;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            //implementing onClickListener
+            itemView.setOnClickListener(this);
+            image = (ImageView)itemView.findViewById(R.id.image);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            mOnGridItemSelectedListener.onGridItemClick(view,getAdapterPosition() - HEADER_PAGER_LAYOUT);
+        }
+    }
+
+
+
+
+
+}
