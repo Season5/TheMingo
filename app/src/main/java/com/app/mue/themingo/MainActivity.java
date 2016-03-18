4633976baab9dfb9c991f7b05c97f020dadfe210
Onikah <mona.kwamboka@gmail.com>
2016-03-18 17:22:37 +0300

    bugs fixed app running

--- app/src/main/java/com/app/mue/themingo/MainActivity.java
+++ app/src/main/java/com/app/mue/themingo/MainActivity.java
@@ -13,7 +13,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.app.mue.themingo.adapters.MainAdapter;
+import com.app.mue.themingo.listeners.OnGridItemSelectedListener;
+import com.app.mue.themingo.listeners.OnPageItemSelectedListener;
+import com.app.mue.themingo.views.GridMarginDecoration;
+
+public class MainActivity extends AppCompatActivity implements OnPageItemSelectedListener, OnGridItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,35 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         setSupportActionBar(toolbar);
+
+        final MainAdapter adapter = new MainAdapter(this, R.array.icons);
+
+        final GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return adapter.isPositionHeader(position) ? layoutManager.getSpanCount() : 1;
+            }
+        });
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new GridMarginDecoration(this, 2, 2, 2, 2));
+
+        recyclerView.setAdapter(adapter);
+
+
+
+    }
+
+    @Override
+    public void onGridItemClick(View v, int position) {
+        Toast.makeText(MainActivity.this, "Grid item clicked position is " + position, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onPagerItemClick(View view, int position) {
+        Toast.makeText(MainActivity.this,"Pager item clicked position is "+position,Toast.LENGTH_SHORT).show();
 
     }
 }
